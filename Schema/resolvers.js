865b62b65739a71d7eeaff46bf7c2186b6e41987
Sanbedan Paul <sanbedan9989@gmail.com>
2024-05-16 00:32:26 +0530

    Basic advance finding user by args

--- Schema/resolvers.js
+++ Schema/resolvers.js
@@ -1,17 +1,29 @@
 //We can start by cretaing a object named resolvers which we will pass to index.js.It will contIN ALL OF THE FUNCTIONS that will exist in our API.
 
 const { UserList } = require("../FakeData.js");
-
+//IGNORE LODASH-  JUST FOR SEARCHING OF AN USER IN THE LOCAL DATABASE
+const lodash = require("lodash");
 //All of the functions that make call to database,the functions that decide what we return to the frontend,All the function that actually do something in our api will exist inside of single resolvers object
 const resolvers = {
   // Inside of the Query type we define all the resolver functions that will exist as a subfield for the query type
   // We should always define the highest level field first like Query inside of our object
   Query: {
     //We can create a function that will return all of the users in our database
-    users() {
+    users: async () => {
       // Inside it we will write all the js script necessary to tell graphql what we want to return
       // If we have a database,This is where we call the database for all the users
       return UserList;
+    },
+
+    ////DEALING WITH ARGUMENTS IN GRAPHQL
+    ////PARENT GIVES YOU THE VALUE THAT WAS RESOLVED BY A PARENT IN THE CHAIN OF TYPES
+    ////ARGS IS AN OBJECT THAT CONTAINS WHATEVER DATA USER SENDS/PASSES AS AN ARGUMENT TO THE QUERY
+    ////WE CAN ACCESS THE ARGS AS args.id
+    // user: async (parent,args) => {        args.id},
+    user: async (_, args) => {
+      const id = args.id;
+      const user = lodash.find(UserList, { id: Number(id) });
+      return user;
     },
   },
 };
--- Schema/typedefs.js
+++ Schema/typedefs.js
@@ -13,7 +13,14 @@ const typeDefs = gql`
     name: String!
     username: String!
     age: Int!
-    nationality: String!
+    nationality: Nationality!
+    friends: [User]
+  }
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   # this type over here will hold as fields all the queries we wanna make inside of our api.
@@ -25,10 +32,27 @@ const typeDefs = gql`
     # Once you define the type user we can start creating queries that deal with type user
 
     users: [User]!
+    ##We can add any arguments that we need to pass inside of this query to work
+    ##We are also Specifing the Type of the argument that is id and iits required so we cant get any user untill id is passed
+    user(id: ID!): User!
     # friends:
 
     # We have to create a resolver that is going to resolve the user field
   }
+
+  ##Enums in GraphQl helps to validate data very easily
+  ##Here we list out all f the possibility that can come be expected as nations
+  ##It is a good practice to wriite enums in capital letters
+  ##We can not pass Canada or canada,Or else  it will thorw error the database should have mating data from the nationality that is CANADA
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    USA
+    CHILE
+  }
+  ##now instead of String in Nationality we can send Enum Nationality
 `;
 
 module.exports = { typeDefs };
